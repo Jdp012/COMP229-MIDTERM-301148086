aeab58452c2519bf50ade19a5ed4663b058a3353
jdp012 <jayqatariya@gmail.com>
2022-10-29 14:07:03 -0400

    db uri changed

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://JAY:jay123@cluster0.bhga4ah.mongodb.net/?retryWrites=true&w=majority"
 };
